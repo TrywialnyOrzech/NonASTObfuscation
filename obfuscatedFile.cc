eb6dd84f359622108653b59da0002561b5825a0c
lstojke <lstojke@github.com>
2021-04-28 02:00:26 +0200

    Improved NamesChanger

--- obfuscatedFile.cc
+++ obfuscatedFile.cc
@@ -1,4 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <string>
-using namespace std; void printWords(const vector<string> msg); int globalVar;string globalVarString;int main(){ vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!!"}; printWords(msg); } void printWords(const vector<string> msg) { for (const string& word: msg) { cout << word << " "; } cout << endl; } 
+#include <vector>
+using namespace std; void printWords(const vector<string> msg); int VvUh();int VvUhC;string VvU;int main(){ vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!!"}; printWords(msg); } void printWords(const vector<string> msg) { for (const string VvUhC: msg) { cout << word << " "; } cout << endl; } int VvU(){ return 0; } 
