a0c6b0b36a31ff8bc30d2e75a8098bd045293064
lstojke <lstojke@github.com>
2021-04-29 21:34:32 +0200

    Small functionality improvement - changing names

--- obfuscatedFile.cc
+++ obfuscatedFile.cc
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 void printWords( const vector<string> msg );
-int globalVar;
-string globalVarString;
+int xz();
+int q;
+string dr;
 int main() {
   vector<string> msg{ "Hello", "C++",     "World",
                       "from",  "VS Code", "and the C++ extension!!" };
   printWords( msg );
 }
 void printWords( const vector<string> msg ) {
-  for( const string &word: msg ) {
-    cout << word << " ";
+  for( const string &NUWEK: msg ) {
+    cout << NUWEK << " ";
   }
   cout << endl;
 }
+int xz() { return 0; }
