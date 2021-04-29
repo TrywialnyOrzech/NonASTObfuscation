5d5bb4eff1776bb5618a6336018fd81159bdbef3
lstojke <lstojke@github.com>
2021-04-29 21:02:19 +0200

    Add dynamic linking to gtest

--- src/test/obfuscationTests.cc
+++ src/test/obfuscationTests.cc
@@ -1,41 +1,40 @@
-#include <gtest/gtest.h>
-#include "../obfuscator/Obfuscator.h"
-#include "../obfuscator/SourcesController.h"
 #include "../obfuscator/FilesCompiler.h"
 #include "../obfuscator/NamesChanger.h"
+#include "../obfuscator/Obfuscator.h"
+#include "../obfuscator/SourcesController.h"
+#include <gtest/gtest.h>
 
 using ::testing::Test;
 
-class obfuscationTests : public Test{
-    protected: 
-        void SetUp() override {
-            testFile = "../../../src/test/testFile.cc";
-            targetFile = "../../../src/test/dumpTestFile.cc";
-            obf = new Obfuscator(testFile,targetFile);
-            }
-        Obfuscator* obf;
-        std::string testFile;
-        std::string targetFile;
-
+class obfuscationTests : public Test {
+protected:
+  void SetUp() override {
+    testFile = "../../../src/test/testFile.cc";
+    targetFile = "../../../src/test/dumpTestFile.cc";
+    obf = new Obfuscator(testFile, targetFile);
+  }
+  Obfuscator *obf;
+  std::string testFile;
+  std::string targetFile;
 };
 
-TEST_F(obfuscationTests, compile_source_file){
-    FilesCompiler compiler(testFile,targetFile);
-    obf = &compiler;
-    ASSERT_TRUE(obf->initialCompilation());
+TEST_F(obfuscationTests, compile_source_file) {
+  FilesCompiler compiler(testFile, targetFile);
+  obf = &compiler;
+  ASSERT_TRUE(obf->initialCompilation());
 }
 
 // Very last test (deletes Obfuscator)
-TEST_F(obfuscationTests, compile_target_file){
-    NamesChanger changer(testFile,targetFile);
-    obf = &changer;
-    obf->init();
-    obf->changeVariablesNames();
-    obf->close();
-    ASSERT_TRUE(FilesCompiler::fileCompilation(targetFile));
+TEST_F(obfuscationTests, compile_target_file) {
+  NamesChanger changer(testFile, targetFile);
+  obf = &changer;
+  obf->init();
+  obf->changeVariablesNames();
+  obf->close();
+  ASSERT_TRUE(FilesCompiler::fileCompilation(targetFile));
 }
 
 int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
