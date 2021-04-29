df269edfe75ba8d29ae29f6fb3651662cb72db8c
lstojke <lstojke@github.com>
2021-04-29 21:02:19 +0200

    Add dynamic linking to gtest

--- src/obfuscator/FilesCompiler.h
+++ src/obfuscator/FilesCompiler.h
@@ -1,16 +1,17 @@
 #ifndef FilesCompiler_H
 #define FilesCompiler_H
 
-#include <string>
-#include <fstream>
 #include "Obfuscator.h"
-class FilesCompiler : public Obfuscator
-{
+#include <fstream>
+#include <string>
+class FilesCompiler : public Obfuscator {
 public:
-    FilesCompiler(std::string oFP = "../testFile.cc", std::string tFP = "../obfuscatedFile.cc"): Obfuscator(oFP,tFP){}
-    static bool fileCompilation(std::string filename);
-    bool initialCompilation();
-
+  FilesCompiler(std::string oFP = "../testFile.cc",
+                std::string tFP = "../obfuscatedFile.cc")
+      : Obfuscator(oFP, tFP) {}
+  ~FilesCompiler() {}
+  static bool fileCompilation(std::string filename);
+  bool initialCompilation();
 };
 
 #endif
