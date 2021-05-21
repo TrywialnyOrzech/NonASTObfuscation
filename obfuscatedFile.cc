6329b006aa6c2b463fe5495ade0a54b226f6f658
lstojke <lstojke@github.com>
2021-05-21 15:54:39 +0200

    Improve variables names changing Add functions with arguments obfuscation Reduce bugs - not finding duplicates, not parsing colons, semicolons

--- obfuscatedFile.cc
+++ obfuscatedFile.cc
@@ -1,20 +1,4 @@
 #include <iostream>
 #include <string>
 #include <vector>
-using namespace std;
-void printWords( const vector<string> msg );
-int YgZ();
-int YgZHykXYFy();
-string Yg;
-int main() {
-  vector<string> msg{ "Hello", "C++",     "World",
-                      "from",  "VS Code", "and the C++ extension!!" };
-  printWords( msg );
-}
-void printWords( const vector<string> msg ) {
-  for( const string &Yg: msg ) {
-    cout << &Yg << " ";
-  }
-  cout << endl;
-}
-int YgZHykXYFy() { return 0; }
+using namespace std; void printWords( const vector<string> msg ); int FdDik();char aNOaTYR(char QYTCdBq); int bdaCVo;string cLhiOrSORH;int main(){ vector<string> msg{ "Hello", "C++", "World", "from", "VS Code", "and the C++ extension!!" }; printWords( msg ); } void printWords( const vector<string> msg ) { for( const string &rbFkZVks:msg ) { cout << &rbFkZVks<< " "; } cout << FdDik()<< endl; } int FdDik(){ return 0; } char aNOaTYR(char QYTCdBq) { return QYTCdBq+ 1; } 
--- testFile.cc
+++ testFile.cc
@@ -4,22 +4,23 @@
 
 using namespace std;
 
-void printWords(const vector<string> msg);
+void printWords( const vector<string> msg );
 int returnVar();
-
+char printCharacter( char c );
 int globalVar;
 string globalVarString;
 
 int main() {
-  vector<string> msg{"Hello", "C++",     "World",
-                     "from",  "VS Code", "and the C++ extension!!"};
-  printWords(msg);
+  vector<string> msg{ "Hello", "C++",     "World",
+                      "from",  "VS Code", "and the C++ extension!!" };
+  printWords( msg );
 }
 
-void printWords(const vector<string> msg) {
-  for (const string &word : msg) {
+void printWords( const vector<string> msg ) {
+  for( const string &word: msg ) {
     cout << word << " ";
   }
-  cout << endl;
+  cout << returnVar() << endl;
 }
 int returnVar() { return 0; }
+char printCharacter( char c ) { return c + 1; }
