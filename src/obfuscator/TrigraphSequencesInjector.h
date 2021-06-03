969df583b7fd1f574717fdf7ff961a27bf575522
TrywialnyOrzech <natan.orze@gmail.com>
2021-06-03 12:50:34 +0200

    Implement TrigraphSequencesInjector header.

---
+++ src/obfuscator/TrigraphSequencesInjector.h
@@ -0,0 +1,24 @@
+#ifndef TrigraphSequencesInjector_H
+#define TrigraphSequencesInjector_H
+
+#include <regex>
+#include <stdlib.h>
+#include <time.h>
+
+#include "Obfuscator.h"
+
+class TrigraphSequencesInjector : public Obfuscator {
+  std::vector<std::string> foundReplacements;
+  std::vector<std::size_t> foundPositions;
+
+public:
+  std::vector<std::string> findRegexMatches( std::string str, std::regex reg );
+  bool findReplacements();
+  bool findPositions();
+  bool injectTrigraphSequences();
+
+  std::vector<std::string> getFoundReplacements() { return foundReplacements; };
+  std::vector<std::size_t> getFoundPositions() { return foundPositions; };
+};
+
+#endif
