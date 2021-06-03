a079f124ab7d738323ab432d2edbb403352c7550
TrywialnyOrzech <natan.orze@gmail.com>
2021-06-03 12:35:50 +0200

    Trigraph sequence experiments. Create files.

---
+++ trigraph_sequences_test_files/test_tri.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string code = "#include <iostream>\nusing namespace std;\n\nint main() {\nint x[4];\n// ZNAK ^\n//ZNAK |\n //ZNAK ~\n}";
+	cout << code << endl;
+	
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+// 	cout << "1: Proba [: " << "??(" << endl;		JEST
+// 	cout << "2: Proba ]: " << "??)" << endl;		JEST
+// 	cout << "3: Proba {: " << "??<" << endl;		JEST
+// 	cout << "4: Proba }: " << "??>" << endl;		JEST
+// 	cout << "5: Proba #: " << "??=" << endl;		JEST
+// 	cout << "7: Proba ^: " << "??'" << endl;		JEST
+// 	cout << "8: Proba |: " << "??!" << endl;		JEST
+// 	cout << "9: Proba ~: " << "??-" << endl;		JEST
+
+// 	// ??(
+// 	// ??-
+
+// 	return 0;
+// } "
+
+	// ==============================================================
+	
+
+	return 0;
+}
